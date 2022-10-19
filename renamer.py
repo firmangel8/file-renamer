d0b35d3c4d3054ca5c960b181544d11666d94eee
pagedev <firmangel8@gmail.com>
2022-10-19 19:06:43 +0800

    add new feature for starting point

--- renamer.py
+++ renamer.py
@@ -1,3 +1,4 @@
+from ast import arg
 import os
 import pathlib
 import argparse
@@ -7,14 +8,15 @@ def flag_ok(value):
     return '\x1b[6;30;42m' + value + '\x1b[0m'
 
 
-def main(folder_name, label_name, n_number):
+def main(folder_name, label_name, n_number, s_start):
     folder = folder_name
     label = label_name
     n = int(n_number)
+    s = int(s_start)
     print("Start renaming..")
     for count, filename in enumerate(os.listdir(folder)):
         file_extension = pathlib.Path(filename).suffix
-        dst = f"{str(label)}_{str(count).zfill(n)}{str(file_extension)}"
+        dst = f"{str(label)}_{str(count+s).zfill(n)}{str(file_extension)}"
         src = f"{folder}/{filename}"
         dst = f"{folder}/{dst}"
         print(dst, flag_ok("SAVED"))
@@ -31,10 +33,11 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--Input", help="Show Output")
     parser.add_argument("-l", "--Label", help="Show Output")
     parser.add_argument("-n", "--NumberPad", help="Show Output", default=3)
+    parser.add_argument("-s", "--NumberStart", help="Show Output", default=0)
 
     # Read arguments from command line
     args = parser.parse_args()
     if args.Input and args.Label:
-        main(args.Input, args.Label, args.NumberPad)
+        main(args.Input, args.Label, args.NumberPad, args.NumberStart)
     else:
         print("python renamer.py -h")
